d5bc2fb224a6fc0c8755dda6aef624f15920dd6f
jamesandere <jamesandere56@gmail.com>
2022-03-03 13:13:25 +0300

    Commit client-side

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,37 +1,38 @@
-const User = require('../models/User');
-const router = require('express').Router();
+const User = require("../models/User");
+const router = require("express").Router();
 
-router.get("/", async(req, res) => {
-    const q = req.query.q;
-    try {
-        const users = await User.find();
-        const keys = Object.assign({}, ...users);
-        const keysWithoutId = Object.keys(keys._doc).filter(x => 
-            x !== "_id" && x !== "__v"    
-        );
+router.get("/", async (req, res) => {
+  const q = req.query.q;
+  try {
+    const users = await User.find();
+    const keys = Object.assign({}, ...users);
+    const keysWithoutId = Object.keys(keys._doc).filter(
+      (x) => x !== "_id" && x !== "__v"
+    );
 
-        const filteredUsers = users.filter(user=> 
-            keysWithoutId.some(key => user[key].toLowerCase().includes(q)))
-        res.status(200).json(filteredUsers.splice(0,10));
-    } catch (error) {
-        res.status(500).json(error);
-    }
+    const filteredUsers = users.filter((user) =>
+      keysWithoutId.some((key) => user[key].toLowerCase().includes(q))
+    );
+    res.status(200).json(filteredUsers.splice(0, 10));
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post("/", async(req, res) => {
-    const newUser = new User({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        gender: req.body.gender
-    });
+router.post("/", async (req, res) => {
+  const newUser = new User({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    gender: req.body.gender,
+  });
 
-    try {
-        const savedUser = await newUser.save();
-        res.status(200).json(savedUser);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
